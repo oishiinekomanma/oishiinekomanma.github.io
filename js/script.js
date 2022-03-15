9fb6b1ba1c479a5c750052f5315c4ea820e75eef
Nekomanma <101625629+oishiinekomanma@users.noreply.github.com>
2022-03-15 16:34:49 +0900

    add files

---
+++ js/script.js
@@ -0,0 +1,47 @@
+$(function () {
+
+  //ページ内スクロール
+  var navHeight = $(".header").outerHeight();
+
+  $('a[href^="#"]').on("click", function () {
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? "html" : href);
+    var position = target.offset().top - navHeight;
+    $("html, body").animate({ scrollTop: position, }, 300, "swing");
+    return false;
+  });
+
+  //ページトップ
+  $("#js-page-top").on("click", function () {
+    $("body,html").animate({ scrollTop: 0, }, 300);
+    return false;
+  });
+
+  /*
+  window.addEventListener("load", function () {
+    $(".mv").addClass("is-active");
+  })
+  */
+
+  // animation呼び出し
+  if ($(".js-scroll-trigger").length) {
+    scrollAnimation();
+  }
+
+  // animation関数
+  function scrollAnimation() {
+    $(window).scroll(function () {
+        $(".js-scroll-trigger").each(function () {
+            let position = $(this).offset().top,
+                scroll = $(window).scrollTop(),
+                windowHeight = $(window).height();
+
+            if (scroll > position - windowHeight + 200) {
+                $(this).addClass("is-active");
+            }
+        });
+    });
+  }
+  $(window).trigger('scroll');
+
+});
